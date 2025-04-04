4aa8dec8ff714a7e2ce06e351a6b7a6817192ad6
elantoinebasset <antoine.basset17@orange.fr>
2025-04-04 12:40:25 +0200

    update_Animation_Image_Button

--- TPPages.js
+++ TPPages.js
@@ -1,20 +1,26 @@
 document.querySelectorAll(".card").forEach(card => {
     card.addEventListener("click", function() {
-        // Créer un placeholder pour maintenir l'espace
+
         const placeholder = document.createElement('div');
         placeholder.style.width = this.offsetWidth + 'px';
         placeholder.style.height = this.offsetHeight + 'px';
         placeholder.style.display = 'inline-block';
+        placeholder.style.position = 'absolute';
         this.parentNode.insertBefore(placeholder, this);
-        
-        
-        this.style.transform = "scale(5.0)";
-        this.style.position = "fixed";
-        this.style.top = "35%";
-        this.style.left = "35%";
-        this.style.transition = "transform 3s ease-in-out";
-        this.style.zIndex = "1000";
-        
+
+
+            this.style.top = "50%"; 
+            this.style.left = "45%";
+
+
+
+        setTimeout(() => {
+            this.style.transform = "scale(5.0)";
+            this.style.position = "fixed";
+            this.style.transition = "transform 2s cubic-bezier(0.68, -0.55, 0.27, 1.55)";
+            this.style.zIndex = "1000";
+        }, 200);  
+
         setTimeout(() => {
             window.location.href = "/Pages/China.html";
         }, 5000);
